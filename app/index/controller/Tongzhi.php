7cd90144bdad5e6b74a25a85c5825b82d205db31
ay37243145 <rong285963>
2020-10-24 19:39:54 +0800

    2020年10月24日 19:39:25

--- app/index/controller/Tongzhi.php
+++ app/index/controller/Tongzhi.php
@@ -27,6 +27,20 @@ class Tongzhi extends BaseController
     }
 
     /*
+     * 发布院级培训通知
+     * */
+    public function add_yjpxtz(){
+        return View::fetch();
+    }
+
+    /*
+     * 发布科室培训通知
+     * */
+    public function add_kspxtz(){
+        return View::fetch();
+    }
+
+    /*
      * 查看医院公告
      * */
     public function look_yygg(){
@@ -34,8 +48,22 @@ class Tongzhi extends BaseController
     }
 
     /*
+     * 查看院级培训通知
+     * */
+    public function look_yjpxtz(){
+        return View::fetch();
+    }
+
+    /*
+     * 查看科室培训通知
+     * */
+    public function look_kspxtz(){
+        return View::fetch();
+    }
+
+    /*
      * 上传文件
-     * 上传头像
+     * 上传附件
      * */
     public function upload_files(){
         $data = upload_file('file','files');
@@ -61,12 +89,80 @@ class Tongzhi extends BaseController
     }
 
     /*
+     * 提交院级培训
+     * */
+    public function add_yjpx_list(){
+        $data = Request::post();
+        $data['fbrID'] = session('user_id');
+        $user_info = get_user_info();
+        $data['fbsj'] = date('Y-m-d H:i:s',time());
+        $data['lx'] = 1;
+        $data['fbr'] = $user_info['name'];
+        $save = Db::name('pxtz_list')->save($data);
+        if($save){
+            return json(rMsg(1,'添加成功'));
+        }else{
+            return json(rMsg(2,'添加失败'));
+        }
+    }
+
+    /*
+     * 提交科室培训
+     * */
+    public function add_kspx_list(){
+        $data = Request::post();
+        $data['fbrID'] = session('user_id');
+        $user_info = get_user_info();
+        $data['fbsj'] = date('Y-m-d H:i:s',time());
+        $data['lx'] = 2;
+        $data['fbr'] = $user_info['name'];
+        $save = Db::name('pxtz_list')->save($data);
+        if($save){
+            return json(rMsg(1,'添加成功'));
+        }else{
+            return json(rMsg(2,'添加失败'));
+        }
+    }
+
+    /*
      * 获取医院公告接口
      *
      * */
     public function yygg_list(){
         $limit = Request::param('limit');
         $list['data'] = Db::name('tongzhi_list')->where('type',1)->order('id', 'desc')->paginate($limit);
+        if($list){
+            $list['code'] = 0;
+            $list['msg'] = '请求成功';
+        }else{
+            $list['code'] = 1;
+            $list['msg'] = '请求失败';
+        }
+        return json($list);
+    }
+
+    /*
+     * 获取院级培训通知接口
+     * */
+    public function yjpxtz_list(){
+        $limit = Request::param('limit');
+        $list['data'] = Db::name('pxtz_list')->where('lx',1)->order('id', 'desc')->paginate($limit);
+        if($list){
+            $list['code'] = 0;
+            $list['msg'] = '请求成功';
+        }else{
+            $list['code'] = 1;
+            $list['msg'] = '请求失败';
+        }
+        return json($list);
+    }
+
+    /*
+     * 获取科室培训通知接口
+     * */
+    public function kspxtz_list(){
+        $limit = Request::param('limit');
+        $list['data'] = Db::name('pxtz_list')->where('lx',2)->order('id', 'desc')->paginate($limit);
         if($list){
             $list['code'] = 0;
             $list['msg'] = '请求成功';
@@ -90,11 +186,25 @@ class Tongzhi extends BaseController
     }
 
     /*
-     * 获取文件列表
+     * 培训通知信息
+     * */
+    public function pxtz_info($id){
+        $info = Db::name('pxtz_list')->find($id);
+        View::assign('title',$info['title']);
+        View::assign('text',$info['text']);
+        View::assign('id',$id);
+        View::assign('zjr',$info['zjr']);
+        View::assign('cyr',$info['cyr']);
+        View::assign('zysx',$info['zysx']);
+        return View::fetch();
+    }
+
+    /*
+     * 获取医院公告文件列表
      * */
     public function file_list($id){
         $list = Db::name('tongzhi_list')->find($id);
-        if($list){
+        if($list['files']!=''){
             $file_lis_data = explode(',',$list['files']);
             foreach ($file_lis_data as $key => $value){
                 $data = explode('/',$value);
@@ -104,6 +214,10 @@ class Tongzhi extends BaseController
             $data_list['code'] = 0;
             $data_list['msg'] = '请求成功';
             $data_list['data'] = $file_list;
+        }else if($list['files']==''){
+            $data_list['code'] = 0;
+            $data_list['msg'] = '请求成功';
+            $data_list['data'] = [];
         }else{
             $data_list['code'] = 1;
             $data_list['msg'] = '请求失败';
@@ -111,6 +225,55 @@ class Tongzhi extends BaseController
         return json($data_list);
     }
 
+    /*
+     * 获取培训通知文件列表
+     * */
+    public function pxtz_file_list($id){
+        $list = Db::name('pxtz_list')->find($id);
+        if($list['files']!=''){
+            $file_lis_data = explode(',',$list['files']);
+            foreach ($file_lis_data as $key => $value){
+                $data = explode('/',$value);
+                $file_list[$key]['name'] = $data[4];
+                $file_list[$key]['path'] = $value;
+            }
+            $data_list['code'] = 0;
+            $data_list['msg'] = '请求成功';
+            $data_list['data'] = $file_list;
+        }else if($list['files']==''){
+            $data_list['code'] = 0;
+            $data_list['msg'] = '请求成功';
+            $data_list['data'] = [];
+        }else{
+            $data_list['code'] = 1;
+            $data_list['msg'] = '请求失败';
+        }
+        return json($data_list);
+    }
+
+    /*
+     * 医院公告点击加一
+     * */
+    public function yygg_dj($id){
+        $updata = Db::name('tongzhi_list')->where('id',$id)->inc('read_num')->update();
+        if($updata){
+            return json(1);
+        }else{
+            return json(2);
+        }
+    }
+
+    /*
+     * 培训通知点击加一
+     * */
+    public function pxtz_dj($id){
+        $updata = Db::name('pxtz_list')->where('id',$id)->inc('ydsl')->update();
+        if($updata){
+            return json(1);
+        }else{
+            return json(2);
+        }
+    }
 
     /*
      * 测试专用
